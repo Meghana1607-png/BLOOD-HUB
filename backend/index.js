a65112c45ec39fa4d271c0f8adedb2a443d65e73
Meghana1607-png <meghanavajrapu2007@gmail.com>
2025-03-11 13:02:15 +0530

    hello

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,7 @@ const nodemailer = require("nodemailer");
 const { name } = require("ejs");
 
 const app = express();
-const PORT = 5000;
+const PORT = 3000;
 
 app.use(cors({}));
 app.use(bodyParser.json());
